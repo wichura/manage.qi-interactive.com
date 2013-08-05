d925f2522f79f612d2944d15f7717eb36cf38b4e
Marcin Wiatr <mail.wichura@gmail.com>
2013-08-05 08:10:16 +0100

    Updated index.php file to use Mata
    Signed-off-by: Marcin Wiatr <mail.wichura@gmail.com>

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <?php
+
+$application = dirname(__FILE__) . "/mata/components/MataWebApplication.php";
+
 // change the following paths if necessary
 $yii = dirname(__FILE__) . '/yii/framework/yii.php';
 
-if (strripos($_SERVER['SERVER_NAME'], "localhost") == true || strrpos($_SERVER['SERVER_NAME'], ".local") == true) {
+if (strripos($_SERVER['SERVER_NAME'], "localhost") == true || strrpos($_SERVER['SERVER_NAME'], ".local") == true ||
+        strrpos($_SERVER['SERVER_NAME'], "qi-interactive.com") == true) {
     $config = dirname(__FILE__) . '/protected/config/dev.php';
     defined('YII_DEBUG') or define('YII_DEBUG', true);
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 } else {
-    defined('YII_DEBUG') or define('YII_DEBUG', false);
     $config = dirname(__FILE__) . '/protected/config/prod.php';
 }
 
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
-
 require_once($yii);
-Yii::createWebApplication($config)->run();
+require_once($application);
+Yii::createApplication("MataWebApplication", $config)->run();
